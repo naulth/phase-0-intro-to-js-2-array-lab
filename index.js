44945e354a1b013517e74e0f0d52957cb8cf313a
Hayden Nault <naulth@gmail.com>
2022-12-27 19:02:22 -0500

    commit tree 7353812ab5b44d2b1e6a81f79a87c6d6f73d4287

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+]
+
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(Ralph){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(Bob){
+    cats.shift()
+}
+
+function appendCat(Broom){
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat(Arnold){
+    const copyOfCats = [ "Arnold", ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat(cat){
+    const newCopyOfCats = cats.slice(0, cats.length - 1);
+    return newCopyOfCats;
+}
+
+function removeFirstCat(cat){
+    const newCopyOfCats = cats.slice(1);
+    return newCopyOfCats;
+}
